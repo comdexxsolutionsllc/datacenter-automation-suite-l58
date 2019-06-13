ebc743a7b2af20f7eb8b51793d5b9be9ff26c60f
A Renner <comdexxsolutionsllc@users.noreply.github.com>
2019-06-13 18:36:55 -0500

    Create LICENSE (#1)

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-06-13 18:25:22.
+    * Generated on 2019-06-13 18:38:00.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
